edd2f8f71579aa310b5273bf94945e88613f29fa
RaulHinojosa24 <raul.hinojosa.perez@gmail.com>
2022-04-05 13:00:30 +0200

    solution

--- js/app.js
+++ js/app.js
@@ -6,3 +6,23 @@ const pictures = [
   "contBcg-4"
 ];
 
+Vue.createApp({
+  data() {
+    return {
+      image: "contBcg-0",
+      index: 0
+    }
+  },
+  methods: {
+    moveSlider(value) {
+      this.index += value;
+      if (this.index < 0) {
+        this.index = pictures.length - 1;
+      } else if (this.index >= pictures.length) {
+        this.index = 0;
+      }
+
+      this.image = pictures[this.index];
+    }
+  }
+}).mount("#app");
